11472a63da4fb995b120a726ba1ab7d7fba93e28
ashton <luckyguy73@yahoo.com>
2017-03-13 14:17:43 -0700

    fix algolia search

--- resources/views/listings/partials/search.blade.php
+++ resources/views/listings/partials/search.blade.php
@@ -1,10 +1,10 @@
 @if(isset($category))
     <listing-search category-id="{{ $category->id }}" :area-ids="{{ 
         $area->descendants->pluck('id')->push($area->id) }}">
+        <hr>
+    <span class="pull-right">powered by <img src="/images/algolia-logo.jpg"></span>
     </listing-search>
 @else
     <listing-search :area-ids="{{ $area->descendants->pluck('id')->push($area->id) }}">
     </listing-search>
-    <hr>
-    <span class="pull-right">powered by <img src="/public/images/algolia-logo.jpg"></span>
 @endif
